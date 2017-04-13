bb7d44ab05d8d6bf9f132ac8d2278a9bb9139169
Ziyao Wang <tonywangziyao@gmail.com>
2017-04-13 17:18:02 -0400

    added

--- instacrawler.py
+++ instacrawler.py
@@ -2,6 +2,7 @@ import os
 
 # starting folder name. auto starts with 0
 counter = 0
+num_craw = -1
 instaUsername = ""
 instaPassword = ""
 
@@ -10,12 +11,20 @@ def run():
     f = open("list.txt")
     name = f.readline().rstrip('\n')
     while name != "":
+        #check total number of pics to craw
+        if num_craw == -1:
+            num_craw = name
+            name = f.readline().rstrip('\n')
+            continue
+        #check username
         if instaUsername == "":
             instaUsername = name
+            name = f.readline().rstrip('\n')
             continue
-
-        if instaPassword == ""
+        #check password
+        if instaPassword == "":
             instaPassword = name
+            name = f.readline().rstrip('\n')
             continue
 
         dirNum = str(counter)
@@ -23,11 +32,11 @@ def run():
         #hashtag
         if name[0] == "#":
             name = name[1:]
-            para = name+" ./"+dirNum+" -n 400 -c "+instaUsername+":"+instaPassword
+            para = name+" ./"+dirNum+" -n "+num_craw+" -c "+instaUsername+":"+instaPassword
             systemCommand = "python -m instaLooter hashtag "
         #profile
         else:
-            para = name+" ./"+dirNum+" -n 400"
+            para = name+" ./"+dirNum+" -n "+num_craw
             systemCommand = "python -m instaLooter "
 
         os.system(systemCommand+para)
